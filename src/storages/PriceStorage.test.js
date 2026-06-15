6b1d6232674a25c791b1dcd3e93f2b83a6a0f153
viktor.makarov <viktor.makarov@myoffice.team>
2026-06-15 23:24:27 +0300

    add price history table

--- src/storages/PriceStorage.test.js
+++ src/storages/PriceStorage.test.js
@@ -1,22 +1,24 @@
 import Database from "better-sqlite3";
 import { CurrencyStorage } from "./CurrencyStorage";
 import { PriceStorage } from "./PriceStorage";
+import { Logger } from "../utils/Logger";
 
 let priceStorage;
 let db;
 
 beforeAll(() => {
+  const logger = new Logger();
   db = new Database(":memory:", {
     verbose: console.log,
   });
-  priceStorage = new PriceStorage(console.log, db);
+  priceStorage = new PriceStorage(logger, db);
 });
 
 beforeEach(() => {
   db.exec("DELETE FROM prices");
 });
 
-describe("priceStorage", () => {
+describe("priceStorage price table", () => {
   it("find should return item with desired symbol", () => {
     priceStorage.create({ symbol: "BTCTUSD", price: "74718" });
     priceStorage.create({ symbol: "ETHUSD", price: "542" });
@@ -39,5 +41,67 @@ describe("priceStorage", () => {
 
     const updated = db.prepare("SELECT * FROM prices").all();
     expect(updated).toEqual(newPrices);
+  });
+});
+
+describe("priceStorage price_history table", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  beforeEach(() => {
+    db.exec("DELETE FROM price_history");
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it("getHistory should return all rows for desired symbol", () => {
+    priceStorage.addToHistory({
+      symbol: "BTCTUSD",
+      price: "74718",
+      date: Date.now(),
+    });
+    priceStorage.addToHistory({
+      symbol: "BTCTUSD",
+      price: "542",
+      date: Date.now(),
+    });
+
+    expect(priceStorage.getHistory("BTC")["BTCTUSD"]).toHaveLength(2);
+  });
+
+  it("updateAllRecords should write items for history and get record should return relevant object", () => {
+    const oldPrices = [
+      { symbol: "USDETH", price: "111" },
+      { symbol: "USDBTC", price: "222" },
+    ];
+    const time1 = new Date("2024-01-15T10:00:00Z");
+    jest.setSystemTime(time1);
+
+    priceStorage.updateAllRecords(oldPrices);
+
+    const newPrices = [
+      { symbol: "USDETH", price: "333" },
+      { symbol: "USDBTC", price: "444" },
+    ];
+    const time2 = new Date("2025-01-15T10:00:00Z");
+    jest.setSystemTime(time2);
+
+    priceStorage.updateAllRecords(newPrices);
+
+    const history = priceStorage.getHistory("USD");
+
+    const result = {
+      USDETH: [
+        { date: time1.getTime(), price: "111" },
+        { date: time2.getTime(), price: "333" },
+      ],
+      USDBTC: [
+        { date: time1.getTime(), price: "222" },
+        { date: time2.getTime(), price: "444" },
+      ],
+    };
+    expect(history).toEqual(result);
   });
 });
--- src/storages/PriceStorage.ts
+++ src/storages/PriceStorage.ts
@@ -3,6 +3,8 @@ import Database, { Statement } from "better-sqlite3";
 import { ILogger } from "../utils/Logger";
 
 export type Price = { symbol: string; price: string };
+export type PriceHistory = { symbol: string; price: string; date: number };
+export type PriceHistoryRow = { price: string; date: number };
 
 export interface IPriceStorage {
   find: (symbol: string) => Price[];
@@ -17,6 +19,8 @@ export class PriceStorage implements IPriceStorage {
 
   findStmt: Database.Statement;
   createStmt: Database.Statement;
+  addToHistoryStmt: Database.Statement;
+  findHistoryStmt: Database.Statement;
 
   constructor(logger: ILogger, db: Database.Database) {
     this.logger = logger;
@@ -28,24 +32,60 @@ export class PriceStorage implements IPriceStorage {
     )
   `);
 
+    db.exec(`CREATE TABLE IF NOT EXISTS price_history (
+      symbol TEXT NOT NULL,
+      price TEXT NOT NULL,
+      date INTEGER NOT NULL 
+    );
+  `);
+
+    db.exec(
+      `CREATE INDEX IF NOT EXISTS idx_price_history_symbol ON price_history(symbol)`,
+    );
+
     this.createStmt = this.db.prepare(
       "INSERT INTO prices (symbol, price) VALUES (@symbol, @price)",
+    ) as Statement<Price>;
+    this.addToHistoryStmt = this.db.prepare(
+      "INSERT INTO price_history (symbol, price, date) VALUES (@symbol, @price, @date)",
     ) as Statement<Price>;
     this.findStmt = this.db.prepare(
       "SELECT * FROM prices WHERE SUBSTR(symbol, 1, LENGTH(@symbol)) = @symbol",
     );
+    this.findHistoryStmt = this.db.prepare(
+      "SELECT * FROM price_history WHERE SUBSTR(symbol, 1, LENGTH(@symbol)) = @symbol",
+    );
 
     this.updateAllRecords = this.db.transaction((prices: Price[]) => {
       db.exec("DELETE FROM prices");
+      const date = Date.now();
       for (const item of prices) {
         this.create(item);
+        this.addToHistory({ ...item, date });
       }
     });
     this.logger.info("PriceStorage ready!");
   }
 
   private create = (price: Price) => this.createStmt.run(price);
+  private addToHistory = (price: PriceHistory) =>
+    this.addToHistoryStmt.run(price);
 
   public find = (symbol: string): Price[] =>
     this.findStmt.all({ symbol }) as Price[];
+
+  public getHistory = (symbol: string): Record<string, PriceHistoryRow[]> => {
+    const prices = this.findHistoryStmt.all({ symbol }) as PriceHistory[];
+    const result = {} as Record<string, PriceHistoryRow[]>;
+
+    prices.forEach((priceRecord: PriceHistory) => {
+      const { price, symbol, date } = priceRecord;
+      if (symbol in result) {
+        result[symbol].push({ price, date });
+      } else {
+        result[symbol] = [{ price, date }];
+      }
+    });
+    return result;
+  };
 }
